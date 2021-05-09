a832a93714afffaab8ee1c1ad5b301d1e764a9d9
ConanShin <conan.shin@sk.com>
2021-05-09 21:42:25 +0900

    commit about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>이것은 윤석이의 About 페이지 입니다.</h1>
   </div>
 </template>
+
+<script>
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class About extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+.about {
+  background-color: yellow;
+}
+</style>
