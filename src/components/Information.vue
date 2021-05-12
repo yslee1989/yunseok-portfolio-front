ccd3784d0fbd9bcc69c272450e4c56da05ed6a84
Yunseok Lee <yslee1989@gmail.com>
2021-05-12 18:45:36 +0900

    0512 -2

---
+++ src/components/Information.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div>나에 대한 정보</div>
+        <div>
+            <profile :imageSource="image"/>
+            <span>나이 </span>
+            <span>{{myAge}}</span><br>
+            <div>나라: {{country}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {Vue, Component, Prop} from 'vue-property-decorator'
+
+@Component({
+    components: {
+        profile: require('./Profile').default
+    }
+})
+export default class Information extends Vue {
+    @Prop() myAge
+    @Prop() image
+
+    get country() {
+        return this.$store.state.person.location?.country
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div>내사진</div>
+        <img :src="imageSource" alt="내얼굴"/>
+    </div>
+</template>
+
+<script>
+import {Vue, Component, Prop} from 'vue-property-decorator'
+
+@Component
+export default class Profile extends Vue {
+    @Prop() imageSource
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
